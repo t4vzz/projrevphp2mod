cc54c4b6f804e7b02b76f957bb89c2fac888b517
t4vzz <souzza.otavio@gmail.com>
2024-05-16 22:32:37 -0300

    fim aula 1

---
+++ src/controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\controllers; 
+
+class AuthController {
+    public function login() {
+        echo 'Login';
+        
+}}
+
+$loginName = $_GET['loginName'];
+$loginSenha = $_GET['loginSenha'];
+
+print_r($loginName);
+print_r($loginSenha);
+
